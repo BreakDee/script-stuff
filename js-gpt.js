1df99712051eba745b70eebb52ca3682a3b8432d
Not Ili <99596096+BreakDee@users.noreply.github.com>
2023-03-05 11:22:13 +0100

    Create js-gpt.js

---
+++ js-gpt.js
@@ -0,0 +1,18 @@
+const prompt = "Hello, how are you today?";
+const apiKey = "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";
+
+fetch("https://api.openai.com/v1/engine/davinci/completions", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+        prompt: prompt,
+        max_tokens: 50,
+        n: 1,
+        stop: "\n"
+    })
+})
+.then(response => response.json())
+.then(data => console.log(data.choices[0].text));
